a46490584b2fbd5fc1f2b59c11a8ff5208b24cd5
ronajon@mailbox.org <ronajon@mailbox.org>
2025-01-14 17:24:16 +0100

    miryoku for splitkb

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,3 +1,14 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
+RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+CONSOLE_ENABLE = no
+EXTRAFLAGS += -flto
+#MOUSEKEY_ENABLE = no
+AUDIO_ENABLE = no
+BACKLIGHT_ENABLE = no
+ENCODER_ENABLE = no
+MIDI_ENABLE = no
+NKRO_ENABLE = yes
+RGBLIGHT_ENABLE = no
+KEY_LOCK_ENABLE = no
